5ae0a81a8852faac76d873bfab5ca947caf14f59
antsz25 <l20211845@tectijuana.edu.mx>
2024-05-06 15:21:20 -0700

    Deploy en netlify

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const server = require('http').createServer(app);
-
+const compression = require('compression');
+const RateLimit = require('express-rate-limit');
 //Initial configuration
+app.use(RateLimit({
+    windowMs: 1*60*1000, // 1 minute
+    max:20, // 20 requests per minute
+}));
 app.use(bodyParser.json());
+app.use(compression());
 let corsoptions = require('./configurations/cors.configuration');
 app.use(cors(corsoptions));
 app.set("trust proxy", 1);
